c4506a0f7257a48686242ec21c1ddceddead4a4b
Flifli-jm <lili1@live.unc.edu>
2022-02-24 23:03:04 -0500

    change error message

--- guess-flip.js
+++ guess-flip.js
@@ -4,9 +4,10 @@ const args = minimist (process.argv);
 args['call']
 const call = args.call
 if(call == null) {
-    console.error("Error: invalid input")
+    console.error("Error: no input")
+    console.error("Usage: node guess-flip --call=[heads|tails]")
 } else if(call != 'heads' && call != 'tails') {
-    console.error("Error: invalid input")
+    console.error("Error: no input")
     console.error("Usage: node guess-flip --call=[heads|tails]")
 } else {
     console.log(flipACoin(call))
